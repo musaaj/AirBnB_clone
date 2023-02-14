24c4bc3f743f3b8f33024da65dbb27bc3e21a8a7
Musa Ibrahim <musaaj@gmail.com>
2023-02-14 04:02:33 +0100

    Auto Commit

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,6 +2,7 @@
 """test state module"""
 from unittest import TestCase
 from models.state import State
+import os
 
 class TestState(TestCase):
     """test State module"""
@@ -11,6 +12,14 @@ class TestState(TestCase):
         """setupp method"""
         cls.state = State()
         cls.state.name = 'Kogi'
+
+    @classmethod
+    def tearDown(cls):
+        try:
+            os.remove('file.json')
+        except FileNotFoundError:
+            raise FileNotFoundError('File no found')
+
     
     def test_object_created(self):
         """test object is created and """
@@ -24,4 +33,5 @@ class TestState(TestCase):
         state_json = self.state.to_dict()
         new_state = State(**state_json)
         self.assertAlmostEqual(state_json, new_state.to_dict())
+        self.assertTrue(os.path.isfile('file.json'))
 
